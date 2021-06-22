b1a3816e1b8508f33fbc5a4df2ec3a0ba85342de
levg34 <levg34@users.noreply.github.com>
2021-06-22 21:18:14 +0200

    add ambulance

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.129.0'
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js'
 import { getRoad, getRoadParts } from './data.js'
-// import { GUI } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/libs/dat.gui.module'
+import { GUI } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/libs/dat.gui.module'
 // import { MeshPhongMaterial } from 'three'
 
 const models = {}
@@ -54,6 +54,28 @@ const loadModel = url => {
     })
 }
 
+let ambulance
+loadModel('/model/cars/ambulance.glb').then(gltf => {
+    ambulance = gltf.scene
+    ambulance.scale.set(0.2,0.2,0.2)
+    ambulance.position.set(0,0,0.9)
+    ambulance.rotation.y = -Math.PI/2
+    const pointlight = new THREE.PointLight(0xffffff, 2, 5)
+    pointlight.position.set(0,0.6,-1.6)
+    // const gui = new GUI()
+    // const lightFolder = gui.addFolder("Cube")
+    // lightFolder.add(pointlight.position, "x", -2, 2, 0.01)
+    // lightFolder.add(pointlight.position, "y", -2, 2, 0.01)
+    // lightFolder.add(pointlight.position, "z", -2, 2, 0.01)
+
+    // const sphereSize = 0.1
+    // const pointLightHelper = new THREE.PointLightHelper(pointlight, sphereSize)
+    // scene.add( pointLightHelper )
+    
+    ambulance.add(pointlight)
+    scene.add(ambulance)
+})
+
 const loadRP = () => {
     if (models.index > 0) {
         scene.remove(models.current)
@@ -96,7 +118,13 @@ getRoadParts().then(res => {
 
 const animate = function () {
 	requestAnimationFrame(animate)
-
+    if (ambulance !== undefined) {
+        if (ambulance.position.x < 5) {
+            ambulance.position.x += 0.01
+        } else {
+            ambulance.position.x = -5
+        }
+    }
 	renderer.render(scene, camera)
 }
 
