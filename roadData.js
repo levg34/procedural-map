ca894e678d01cb2c1953b94a23ac1199d37e9c07
levg34 <levg34@users.noreply.github.com>
2021-06-25 23:59:40 +0200

    use Vector3 instead of Direction

--- roadData.js
+++ roadData.js
@@ -1,26 +1,45 @@
+const { Vector3 } = require("three")
+
 const turn = {
     for: ['road_bend','road_bendSquare','road_bendSidewalk','road_curve','road_curvePavement'],
-    directions: ['+x','-y']
+    directions: [
+        new Vector3(1,0,0),
+        new Vector3(0,0,-1)
+    ]
 }
 
 const straight = {
     for: ['road_crossing','road_drivewayDouble','road_drivewaySingle','#road_side','#road_sideEntry','#road_sideExit','#road_split','road_straight'],
-    directions: ['+x','-x']
+    directions: [
+        new Vector3(1,0,0),
+        new Vector3(-1,0,0)
+    ]
 }
 
 const allDirections = {
     for: ['road_crossroad','road_crossroadLine','road_crossroadPath','road_roundabout'],
-    directions: ['+x','-x','+y','-y']
+    directions: [
+        new Vector3(1,0,0),
+        new Vector3(-1,0,0),
+        new Vector3(0,0,1),
+        new Vector3(0,0,-1)
+    ]
 }
 
 const tripleIntersection = {
     for: ['road_curveIntersection','road_intersection','road_intersectionLine','road_intersectionPath'],
-    directions: ['+x','-x','-y']
+    directions: [
+        new Vector3(1,0,0),
+        new Vector3(-1,0,0),
+        new Vector3(0,0,-1)
+    ]
 }
 
 const roadEnd = {
     for: ['road_end'],
-    directions: ['-x']
+    directions: [
+        new Vector3(-1,0,0)
+    ]
 }
 
 const noEntry = {
